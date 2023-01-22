8d989b5c70e3926fb72499a4f02b6d3791d34e33
Kevin Ng <kevinwng23@gmail.com>
2023-01-21 22:13:19 -0500

    valid anagram

--- algos.js
+++ algos.js
@@ -35,3 +35,10 @@ var twoSum = function(nums, target) {
    return sum;
    
 };
+
+// -------
+
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
