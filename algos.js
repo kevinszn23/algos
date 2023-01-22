a1a49c581693e911f6086547a0dce7e58b07bd91
Kevin Ng <kevinwng23@gmail.com>
2023-01-21 22:16:54 -0500

    buy and sell stock

--- algos.js
+++ algos.js
@@ -55,3 +55,12 @@ function ana(s, t){
 
 }
 console.log(ana("doggo", "gggod"))
+
+// Best Time to Buy and Sell Stock
+
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
