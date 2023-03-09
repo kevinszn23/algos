0f1e527ad4a1fcc5440dc34203c3c06f7763497c
Kevin Ng <kevinwng23@gmail.com>
2023-03-09 12:28:52 -0500

    group anagrams algo finished

--- algos.js
+++ algos.js
@@ -158,3 +158,14 @@ var romanToInt = function(s) {
 // Given an array of strings strs, group the anagrams together. You can return the answer in any order.
 
 // An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+let groupAnagrams = function(strs) {
+    let obj = {};
+    for (let str of strs) {
+        let letters = str.split("").sort().join("");
+        obj[letters] ? obj[letters].push(str) : obj[letters] = [str];
+    }
+    return Object.values(obj);
+};
+// Time Complexity: O(n*klog(k)) where n is the length of input array and k is the maximum length of a string in input array
+// Space Complexity: O(n)
